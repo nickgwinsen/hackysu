8759651192c9376397a2447cf74e1c4b2b0e716d
nicolasjott <nicobox2002@gmail.com>
2023-04-18 14:02:49 -0400

    fixed crashing on two hands issue

--- server/api/api.py
+++ server/api/api.py
@@ -17,7 +17,7 @@ async def process_image(file: UploadFile = File(...)):
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
 
-    hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.7)
+    hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.7)
 
     labels_dict = {0: 'A', 1: 'L', 2: 'O', 3: 'I', 4: 'H', 5: 'Y', 6: 'K'}
 
--- server/training/inference_classifier.py
+++ server/training/inference_classifier.py
@@ -1,10 +1,9 @@
 import pickle
-
 import cv2
 import mediapipe as mp
 import numpy as np
 
-model_dict = pickle.load(open('./model.p', 'rb'))
+model_dict = pickle.load(open('../model.p', 'rb'))
 model = model_dict['model']
 
 cap = cv2.VideoCapture(0)
@@ -13,7 +12,7 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.65)
+hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.65)
 
 labels_dict = {0: 'A', 1: 'L', 2: 'O', 3: 'I', 4: 'H', 5: 'Y', 6: 'K'}
 while True:
