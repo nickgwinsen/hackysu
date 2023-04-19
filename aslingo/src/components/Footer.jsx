7f2f1befb9ddaa21a31a04fb89613f91fb3c63f9
nicolasjott <nicobox2002@gmail.com>
2023-04-19 11:01:40 -0400

    removed unused imports

--- aslingo/src/components/Footer.jsx
+++ aslingo/src/components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 
